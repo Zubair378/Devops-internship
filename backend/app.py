947fff8e7c6837764fb1f690a5b41f7a892575f0
zubbi don <zubairtari237@gmail.com>
2026-07-20 15:48:09 +0500

    Week 1: devops multistage docker files

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import socket
+
+app = Flask(__name__)
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+
+@app.route('/info')
+def info():
+    return jsonify({
+        "service": "backend",
+        "developer": "Zubair",
+        "version": "1.0.0",
+        "hostname": socket.gethostname()
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
